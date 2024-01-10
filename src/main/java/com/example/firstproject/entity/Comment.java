3467939f1a280361e6dd6f63b472da4a693ec1cc
dthcs <dthcsc@gmail.com>
2024-01-10 23:24:51 +0900

    "17 chapter-comment on web page"

--- src/main/java/com/example/firstproject/entity/Comment.java
+++ src/main/java/com/example/firstproject/entity/Comment.java
@@ -1,11 +1,14 @@
 package com.example.firstproject.entity;
 
+import com.example.firstproject.dto.CommentDto;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Entity
 @Getter
 @ToString
@@ -25,4 +28,33 @@ public class Comment {
     @Column
     private String body;
 
+    public static Comment createComment(CommentDto dto, Article article) {
+        // 예외 발생
+        if (dto.getId() != null)
+            throw new IllegalArgumentException("fail, no id please");
+//        log.info(String.valueOf(dto.getArticleId()));
+//        log.info(String.valueOf(article.getId()));
+        if (dto.getArticleId() != article.getId())
+            throw new IllegalArgumentException("fail, wrong articleId");
+
+        // 엔티티 생성 및 반환
+        return new Comment(
+                dto.getId(),
+                article,
+                dto.getNickname(),
+                dto.getBody()
+        );
+
+    }
+
+    public void patch(CommentDto dto) {
+        // 예외 발생
+        if(this.id != dto.getId())
+            throw new IllegalArgumentException("update failed, input wrong id");
+        // 객체 갱신
+        if(dto.getNickname() != null)
+            this.nickname = dto.getNickname();
+        if(dto.getBody() != null)
+            this.body = dto.getBody();
+    }
 }
